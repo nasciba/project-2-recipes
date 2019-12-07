fa267064809b339e02003dcc31fa605518ecd5f8
marciano <mcnvrodrigues@gmail.com>
2019-12-07 13:28:02 -0300

    verifica se receita existe no banco

--- models/random-recipe.js
+++ models/random-recipe.js
@@ -15,28 +15,40 @@ const randomRecipeSchema = new Schema({
       lowFodmap: Boolean,
       ketogenic: Boolean,
       whole30: Boolean,
-      sourceUrl: String,
-      spoonacularSourceUrl: String,
+      sourceUrl: {
+        type: String,
+        format:"uri"
+        },
+      spoonacularSourceUrl: {
+        type: String,
+        format:"uri"
+        },
       aggregateLikes: Number,
       spoonacularScore: Number,
       healthScore: Number,
       creditsText: String,
       sourceName: String,
       pricePerServing: Number,
-      // extendedIngredients : [{    
-      //       id: Number,
-      //       aisle: String,
-      //       image: String,
-      //       name: String,
-      //       amount: Number,
-      //       unit: String,
-      //       unitShort: String,
-      //       unitLong: String,
-      //       originalString: String,
-      //       metaInformation:[{
-      //       info: String
-      //     }]    
-      // }],
+      extendedIngredients : {
+        type: Array,
+        items:{
+          type: Object,
+          properties: {
+            id: Number,
+            aisle: String,
+            image: {
+              type: String,
+              format:"uri"
+            },
+            name: String,
+            amount: Number,
+            unit: String,
+            unitShort: String,
+            unitLong: String,
+            originalString: String,
+          }
+        }
+      },
       id: Number,
       title: String,
       readyInMinutes: Number,
@@ -53,9 +65,12 @@ const randomRecipeSchema = new Schema({
       occasions: [{
         type: String,
       }],
-      winePairing: [{
-        type: String,
-      }],
+      winePairing: {
+        type : Object,
+        items: {
+          type: Array
+        }
+      },
       instructions: String
 }, {
   timestamps: { createdAt: "created_at", updatedAt: "updated_at" }
--- routes/site-routes.js
+++ routes/site-routes.js
@@ -4,7 +4,6 @@ const router = express.Router();
 const ensureLogin = require("connect-ensure-login");
 const axios = require("axios");
 const RandomRecipe = require("../models/random-recipe");
-const Recipes = require("./data");
 const APIHandler = require("./APIHandler");
 
 const recipesAPI = new APIHandler('http://localhost:8000');
@@ -13,166 +12,319 @@ const recipesAPI = new APIHandler('http://localhost:8000');
       res.render("home");
   });
 
+  router.get("/recipes", (req, res) => {
 
-  router.get("/receitas", /*ensureLogin.ensureLoggedIn(),*/ (req, res) => {
-    
-    let numberOfRecipes = 20; 
-    
+    // faz uma requisição para a API e verifica se a receita retornada
+    // não existe no banco
     recipesAPI.getFullList()
-      .then(responseFromApi => {
-        let recipes = responseFromApi.data;
+    .then(responseFromApi => {
 
-        responseFromApi.data.forEach(recipe =>{
-          console.log(recipe.title);
-          
-        });
-        res.render("receitas", {recipes});
+      responseFromApi.data.forEach(recipe =>{
 
-      })
-      .catch(err => {
+        // titulo da receita vinda da API
+        let titleRecipe = recipe.title;
 
+        // procura receita no banco pelo titulo
+        RandomRecipe.findOne({'title': titleRecipe})
+        .then(rec => {
+
+          // se a receita não existe no banco, ela é inserida
+          if(rec == null){
+
+            console.log('A receita ' + titleRecipe + ' nao existe!')
+            const newRandomRecipe = 
+              new RandomRecipe({
+                vegetarian: recipe.vegetarian,
+                vegan: recipe.vegan,
+                glutenFree: recipe.glutenFree,
+                dairyFree: recipe.dairyFree,
+                veryHealthy: recipe.veryHealthy,
+                cheap: recipe.cheap,
+                veryPopular: recipe.veryPopular,
+                sustainable: recipe.sustainable,
+                weightWatcherSmartPoints: recipe.weightWatcherSmartPoints,
+                gaps: recipe.gaps,
+                lowFodmap: recipe.lowFodmap,
+                ketogenic: recipe.ketogenic,
+                whole30: recipe.whole30,
+                sourceUrl: recipe.sourceUrl,
+                spoonacularSourceUrl: recipe.spoonacularSourceUrl,
+                aggregateLikes: recipe.aggregateLikes,
+                spoonacularScore: recipe.spoonacularScore,
+                healthScore: recipe.healthScore,
+                creditsText: recipe.creditsText,
+                sourceName: recipe.sourceName,
+                pricePerServing: recipe.pricePerServing,
+                extendedIngredients: recipe.extendedIngredients,
+                id: recipe.id,
+                title: recipe.title,
+                readyInMinutes: recipe.readyInMinutes,
+                servings: recipe.servings,
+                image: recipe.image,
+                imageType: recipe.imageType,
+                cuisines: recipe.cuisines,
+                dishTypes: recipe.dishTypes,
+                diets: recipe.diets,
+                occasions: recipe.occasions,
+                winePairing: recipe.winePairing,
+                instructions: recipe.instructions
+              });
+      
+              newRandomRecipe.save((err) => {
+                if (err) {
+                  console.log('erro ao salvar as receitas no banco!', err);
+                } else {
+                  console.log('Sucesso as salvar as receitas no banco!');
+                }
+              });  
+
+          }else{
+            console.log('receita ' + titleRecipe + ' já existe!');
+          }
+        })
+        .catch(err => {
+          console.log(err);
+        });           
       });
+    })
+    .catch(err => {
+        console.log(err);
+    });
 
-    // let recipes = Recipes;
-    // console.log(recipes[0].vegetarian);
+    //retorna todas receitas do banco
+    RandomRecipe.find({})
+    .then(recipes => {
+      res.render("receitas", {recipes});
+    })
+    .catch(err => {
+      console.log(err);
+    })
     
+  });
 
-    // recipes.forEach( recipe => {    
-              
-    //   console.log('DishTypes ', recipe.dishTypes) 
-    //   console.log('Diets ', recipe.diets)      
+  router.get("/receitasteste", (req, res) => {
 
-    //   const newRandomRecipe = new RandomRecipe({
-    //       vegetarian: recipe.vegetarian,
-    //       vegan: recipe.vegan,
-    //       glutenFree: recipe.glutenFree,
-    //       dairyFree: recipe.dairyFree,
-    //       veryHealthy: recipe.veryHealthy,
-    //       cheap: recipe.cheap,
-    //       veryPopular: recipe.veryPopular,
-    //       sustainable: recipe.sustainable,
-    //       weightWatcherSmartPoints: recipe.weightWatcherSmartPoints,
-    //       gaps: recipe.gaps,
-    //       lowFodmap: recipe.lowFodmap,
-    //       ketogenic: recipe.ketogenic,
-    //       whole30: recipe.whole30,
-    //       sourceUrl: recipe.sourceUrl,
-    //       spoonacularSourceUrl: recipe.spoonacularSourceUrl,
-    //       aggregateLikes: recipe.aggregateLikes,
-    //       spoonacularScore: recipe.spoonacularScore,
-    //       healthScore: recipe.healthScore,
-    //       creditsText: recipe.creditsText,
-    //       sourceName: recipe.sourceName,
-    //       pricePerServing: recipe.pricePerServing,
-    //       // extendedIngredients: recipe.extendedIngredients,
-    //       id: recipe.id,
-    //       title: recipe.title,
-    //       readyInMinutes: recipe.readyInMinutes,
-    //       servings: recipe.servings,
-    //       image: recipe.image,
-    //       imageType: recipe.imageType,
-    //       // cuisines: recipe.cuisines,
-    //       dishTypes: recipe.dishTypes,
-    //       diets: recipe.diets,
-    //       occasions: recipe.occasions,
-    //       // winePairing: recipe.winePairing,
-    //       instructions: recipe.instructions
-    //   });
-
+    //recupera todas as receitas do banco
+    RandomRecipe.find({})
+    .then(recipes => {
       
+      //caso não tenha nenhuma receita no banco, buscar receitas da api
+      if (recipes.length == 0){
+
+        recipesAPI.getFullList()
+        .then(responseFromApi => {
+  
+          responseFromApi.data.forEach(recipe =>{
+            const newRandomRecipe = 
+              new RandomRecipe({
+                vegetarian: recipe.vegetarian,
+                vegan: recipe.vegan,
+                glutenFree: recipe.glutenFree,
+                dairyFree: recipe.dairyFree,
+                veryHealthy: recipe.veryHealthy,
+                cheap: recipe.cheap,
+                veryPopular: recipe.veryPopular,
+                sustainable: recipe.sustainable,
+                weightWatcherSmartPoints: recipe.weightWatcherSmartPoints,
+                gaps: recipe.gaps,
+                lowFodmap: recipe.lowFodmap,
+                ketogenic: recipe.ketogenic,
+                whole30: recipe.whole30,
+                sourceUrl: recipe.sourceUrl,
+                spoonacularSourceUrl: recipe.spoonacularSourceUrl,
+                aggregateLikes: recipe.aggregateLikes,
+                spoonacularScore: recipe.spoonacularScore,
+                healthScore: recipe.healthScore,
+                creditsText: recipe.creditsText,
+                sourceName: recipe.sourceName,
+                pricePerServing: recipe.pricePerServing,
+                extendedIngredients: recipe.extendedIngredients,
+                id: recipe.id,
+                title: recipe.title,
+                readyInMinutes: recipe.readyInMinutes,
+                servings: recipe.servings,
+                image: recipe.image,
+                imageType: recipe.imageType,
+                cuisines: recipe.cuisines,
+                dishTypes: recipe.dishTypes,
+                diets: recipe.diets,
+                occasions: recipe.occasions,
+                winePairing: recipe.winePairing,
+                instructions: recipe.instructions
+              });
+          
+              newRandomRecipe.save((err) => {
+                if (err) {
+                  console.log('erro ao salvar as receitas no banco!', err);
+                } else {
+                  console.log('Sucesso as salvar as receitas no banco!');
+                }
+              });     
+          });         
+  
+        })
+        .catch(err => {
+            console.log(err);
+        });  
+      }else{
+        // caso o banco não esteja vazio, fazer nova busca na api por
+        // novas receitas 
+        
+        recipesAPI.getFullList()
+        .then(responseFromApi => {
+  
+          responseFromApi.data.forEach(recipe =>{
+
+            let titleRecipe = recipe.title;
+
+            RandomRecipe.findOne({'title': titleRecipe})
+            .then(rec => {
+              if(rec == null){
+
+                console.log('A receita ' + titleRecipe + ' nao existe!')
+                const newRandomRecipe = 
+                  new RandomRecipe({
+                    vegetarian: recipe.vegetarian,
+                    vegan: recipe.vegan,
+                    glutenFree: recipe.glutenFree,
+                    dairyFree: recipe.dairyFree,
+                    veryHealthy: recipe.veryHealthy,
+                    cheap: recipe.cheap,
+                    veryPopular: recipe.veryPopular,
+                    sustainable: recipe.sustainable,
+                    weightWatcherSmartPoints: recipe.weightWatcherSmartPoints,
+                    gaps: recipe.gaps,
+                    lowFodmap: recipe.lowFodmap,
+                    ketogenic: recipe.ketogenic,
+                    whole30: recipe.whole30,
+                    sourceUrl: recipe.sourceUrl,
+                    spoonacularSourceUrl: recipe.spoonacularSourceUrl,
+                    aggregateLikes: recipe.aggregateLikes,
+                    spoonacularScore: recipe.spoonacularScore,
+                    healthScore: recipe.healthScore,
+                    creditsText: recipe.creditsText,
+                    sourceName: recipe.sourceName,
+                    pricePerServing: recipe.pricePerServing,
+                    extendedIngredients: recipe.extendedIngredients,
+                    id: recipe.id,
+                    title: recipe.title,
+                    readyInMinutes: recipe.readyInMinutes,
+                    servings: recipe.servings,
+                    image: recipe.image,
+                    imageType: recipe.imageType,
+                    cuisines: recipe.cuisines,
+                    dishTypes: recipe.dishTypes,
+                    diets: recipe.diets,
+                    occasions: recipe.occasions,
+                    winePairing: recipe.winePairing,
+                    instructions: recipe.instructions
+                  });
+          
+              newRandomRecipe.save((err) => {
+                if (err) {
+                  console.log('erro ao salvar as receitas no banco!', err);
+                } else {
+                  console.log('Sucesso as salvar as receitas no banco!');
+                }
+              });  
+
+              }else{
+                console.log('receita ' + titleRecipe + ' já existe!');
+              }
+            })
+            .catch();
+
+               
+          });         
+  
+        })
+        .catch(err => {
+            console.log(err);
+        });         
+
+      }
+      res.render("receitas", {recipes});
+    })
+    .catch(); 
+  });
+
+
+  router.get("/receitas", /*ensureLogin.ensureLoggedIn(),*/ (req, res) => {   
     
-    //   newRandomRecipe.save((err, recipe) => {
-    //     // console.log(recipe);
-    //       if (err) {
-    //           console.log('erro ao salvar as receitas no banco!', err);
-    //       } else {
-    //           console.log('Sucesso as salvar as receitas no banco!');
-    //       }
-    //   });
-
-    //   // const newRandomRecipe = new RandomRecipe({ 
-    //   //   $push: { dishTypes: 'teste' } 
-    //   // });
-
-    //   // newRandomRecipe.save().then(recipe => {
-    //   //   console.log(recipe);
-    //   // })
-    // });
-
-        // console.log(recipes[0].vegetarian);
-      
     
-    // axios({
-    //     "method":"GET",
-    //     "url":"https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/random",
-    //     "headers":{
-    //     "content-type":"application/octet-stream",
-    //     "x-rapidapi-host":process.env.FOOD_NUTRITION_HOST,
-    //     "x-rapidapi-key":process.env.FOOD_NUTRITION_KEY
-    //     },"params":{
-    //     "number": `${numberOfRecipes}`,
-    //     "tags":"vegetarian%2Cdessert"
-    //     }
-    //     })
-    //     .then((response)=>{
-    //         let recipes = response.data.recipes;
-    //         // res.send({response});
+    let numberOfRecipes = 1;      
+    
+    axios({
+        "method":"GET",
+        "url":"https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/random",
+        "headers":{
+        "content-type":"application/octet-stream",
+        "x-rapidapi-host":process.env.FOOD_NUTRITION_HOST,
+        "x-rapidapi-key":process.env.FOOD_NUTRITION_KEY
+        },"params":{
+        "number": `${numberOfRecipes}`,
+        "tags":"vegetarian%2Cdessert"
+        }
+        })
+        .then(  (response)=>{
+            let recipes = response.data.recipes;
+            // res.send({response});
 
-    //         recipes.forEach( recipe => {              
+            recipes.forEach( recipe => {              
 
-    //           const newRandomRecipe = new RandomRecipe({
-    //               vegetarian: recipe.vegetarian,
-    //               vegan: recipe.vegan,
-    //               glutenFree: recipe.glutenFree,
-    //               dairyFree: recipe.dairyFree,
-    //               veryHealthy: recipe.veryHealthy,
-    //               cheap: recipe.cheap,
-    //               veryPopular: recipe.veryPopular,
-    //               sustainable: recipe.sustainable,
-    //               weightWatcherSmartPoints: recipe.weightWatcherSmartPoints,
-    //               gaps: recipe.gaps,
-    //               lowFodmap: recipe.lowFodmap,
-    //               ketogenic: recipe.ketogenic,
-    //               whole30: recipe.whole30,
-    //               sourceUrl: recipe.sourceUrl,
-    //               spoonacularSourceUrl: recipe.spoonacularSourceUrl,
-    //               aggregateLikes: recipe.aggregateLikes,
-    //               spoonacularScore: recipe.spoonacularScore,
-    //               healthScore: recipe.healthScore,
-    //               creditsText: recipe.creditsText,
-    //               sourceName: recipe.sourceName,
-    //               pricePerServing: recipe.pricePerServing,
-    //               extendedIngredients: recipe.extendedIngredients,
-    //               id: recipe.id,
-    //               title: recipe.title,
-    //               readyInMinutes: recipe.readyInMinutes,
-    //               servings: recipe.servings,
-    //               image: recipe.image,
-    //               imageType: recipe.imageType,
-    //               cuisines: recipe.cuisines,
-    //               dishTypes: recipe.dishTypes,
-    //               diets: recipe.diets,
-    //               occasions: recipe.occasions,
-    //               winePairing: recipe.winePairing,
-    //               instructions: recipe.instructions
-    //           });
+              const newRandomRecipe = new RandomRecipe({
+                  vegetarian: recipe.vegetarian,
+                  vegan: recipe.vegan,
+                  glutenFree: recipe.glutenFree,
+                  dairyFree: recipe.dairyFree,
+                  veryHealthy: recipe.veryHealthy,
+                  cheap: recipe.cheap,
+                  veryPopular: recipe.veryPopular,
+                  sustainable: recipe.sustainable,
+                  weightWatcherSmartPoints: recipe.weightWatcherSmartPoints,
+                  gaps: recipe.gaps,
+                  lowFodmap: recipe.lowFodmap,
+                  ketogenic: recipe.ketogenic,
+                  whole30: recipe.whole30,
+                  sourceUrl: recipe.sourceUrl,
+                  spoonacularSourceUrl: recipe.spoonacularSourceUrl,
+                  aggregateLikes: recipe.aggregateLikes,
+                  spoonacularScore: recipe.spoonacularScore,
+                  healthScore: recipe.healthScore,
+                  creditsText: recipe.creditsText,
+                  sourceName: recipe.sourceName,
+                  pricePerServing: recipe.pricePerServing,
+                  extendedIngredients: recipe.extendedIngredients,
+                  id: recipe.id,
+                  title: recipe.title,
+                  readyInMinutes: recipe.readyInMinutes,
+                  servings: recipe.servings,
+                  image: recipe.image,
+                  imageType: recipe.imageType,
+                  cuisines: recipe.cuisines,
+                  dishTypes: recipe.dishTypes,
+                  diets: recipe.diets,
+                  occasions: recipe.occasions,
+                  winePairing: recipe.winePairing,
+                  instructions: recipe.instructions
+              });
 
-    //           newRandomRecipe.save((err) => {
-    //             if (err) {
-    //               console.log('erro ao salvar as receitas no banco!', err);
-    //             } else {
-    //               console.log('Sucesso as salvar as receitas no banco!');
-    //             }
-    //           });
+              newRandomRecipe.save((err) => {
+                if (err) {
+                  console.log('erro ao salvar as receitas no banco!', err);
+                } else {
+                  console.log('Sucesso as salvar as receitas no banco!');
+                }
+              });
       
-    //         })
-    //         //res.render("receitas", {recipes});
-    //         res.send(recipes);
-    //         //console.log(recipes[0].diets)
-    //     })
-    //     .catch((error)=>{
-    //       console.log(error)
-    //     });
+            })
+            res.render("receitas", {recipes});
+            //res.send(recipes);
+            //console.log(recipes[0].diets)
+        })
+        .catch((error)=>{
+          console.log(error)
+        });
     // res.render("receitas", {recipes});
     
   });
